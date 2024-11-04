a3075869f19c3efe67f1cdceccae0068834a6400
LJanaszek <lukasjanaszek@gmail.com>
2024-11-04 10:04:14 +0100

    dodawanie, usuwanie z listy

--- src/routes/scenario1/p3.tsx
+++ src/routes/scenario1/p3.tsx
@@ -4,7 +4,6 @@ import { getGameRoute, PAGES } from "..";
 import PageText from "../../components/pageText";
 import './style.module.scss'
 import { Link } from "react-router-dom";
-import Notepad from "../../components/notepad";
 
 export interface GameMapPoint extends PointData {
     pointId: string
@@ -14,36 +13,64 @@ export default function P3() {
 
     const [textList, setTextList] = useState<string[]>([])
 
+    const words :{[key: string]: string} = {
+        Lorem : 'dupa dupa',
+        ipsum : 'ipsum dolor',
+        dolor : 'dolor sit',
+        sit : 'sit amet',
+    }
+    const addTextToList = (text: string) => {
+        if (!textList.includes(words[text]) && textList.length < 5) {
+            setTextList([...textList, words[text]])
+        }
+    }
+    const removeTextFromList = (text: any) => {
+        //adding elements to list according to words keys
+        if (textList.includes(text)) {   
+            setTextList(textList.filter(item => item !== text))
+        }
+    }
     useEffect(() => {
-        onLoadUpdate();
-    }, [])
-    function onLoadUpdate() {
         if (localStorage.getItem('textList')) {
             setTextList(JSON.parse(localStorage.getItem('textList')!));
+            localStorage.removeItem('textList')
         }
-    }
-    // onLoadUpdate();
-
-    const addTextToList = (text: string) => {
-        if (!textList.includes(text) && textList.length < 5) {
-            setTextList([...textList, text])
-
-        }
-    }
-
+    }, [textList])
     return (<div>
         <section>
             <PageText
                 image="">
                 <p>
                     <span onClick={(e) =>
-                        addTextToList(e.currentTarget.innerText)}>Lorem</span>
-                        <span onClick={(e) => addTextToList(e.currentTarget.innerText)}>ipsum</span>
-                    <span onClick={(e) => addTextToList(e.currentTarget.innerText)}>dupa</span>ipsum dolor sit amet consectetur adipisicing elit. Maxime maiores, eum nesciunt aut autem nam. Nemo dolores inventore expedita sunt praesentium cupiditate ipsum a consectetur.
-                    <span onClick={(e) => addTextToList(e.currentTarget.innerText)} >Praesentium</span>
-                    <span onClick={(e) => addTextToList(e.currentTarget.innerText)}>345678</span>Nobis sit aspernatur autem tempore!</p>
+                        addTextToList(e.currentTarget.innerText)}>
+                        Lorem
+                    </span>
+                    <span onClick={(e) =>
+                        addTextToList(e.currentTarget.innerText)}>
+                        ipsum
+                    </span>
+                    <span onClick={(e) =>
+                        addTextToList(e.currentTarget.innerText)}>
+                        dolor
+                    </span>
+                    ipsum dolor sit amet consectetur adipisicing elit. Maxime maiores, eum nesciunt aut autem nam. Nemo dolores inventore expedita sunt praesentium cupiditate ipsum a consectetur.
+                    <span onClick={(e) =>
+                        addTextToList(e.currentTarget.innerText)}>
+                        sit
+                    </span>
+                    <span onClick={(e) =>
+                        addTextToList(e.currentTarget.innerText)}>
+                        12345678
+                    </span>
+                    Nobis sit aspernatur autem tempore! </p>
             </PageText>
-            <Notepad wordsList={textList} />
+            <div>
+                {textList.map((text, index) =>
+                    <li key={index}
+                        onClick={(e) => removeTextFromList(e.currentTarget.innerText)}>
+                        {text}
+                    </li>)}
+            </div>
         </section>
         <Link onClick={() => {
             localStorage.setItem('textList', JSON.stringify(textList));
