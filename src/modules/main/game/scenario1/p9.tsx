149817318a9804a70d5d7535d8e1455735c15142
LJanaszek <lukasjanaszek@gmail.com>
2024-11-26 22:06:08 +0100

    uruchomienie bazy danych json

--- src/modules/main/game/scenario1/p9.tsx
+++ src/modules/main/game/scenario1/p9.tsx
@@ -1,26 +1,41 @@
 import { Link } from "react-router-dom";
-import data from "./p9.json"
-
 import { useState } from "react";
+
 import { getGameRoute, PAGES } from "../../../../routes";
 export default function P9() {
-    const checkbox = data.checkbox
+    //fetch data from p9.json file with utf-8 pl encoding
+    const data = require('./p9.json');
+    let currentTable: { [key: string]: any } = {}
+    let keys: any[] = []
     const [points, setPoints] = useState(0);
+
+    console.log(Object.keys(data));
+    Object.keys(data).forEach((key: any) => {
+        // console.log(key);
+        currentTable = ((data as { [key: string]: any })[key]);
+    })
+    Object.keys(currentTable).forEach((key: any) => {
+        keys.push(key)
+    })
+    let number = 0
+
+    if (localStorage.getItem('textList')) {
+
+    }
+
     return (
-        <div>
-            {Object.values(checkbox).map((values, key) => {
-                return (
-                    <div key={key}>
-                       
-                        
-                    </div>
-                );
+
+        <form>
+            {keys.map((key: any) => {
+                console.log(currentTable[key].name);
+                return <label key={key}>
+                    <input type="radio" key={key} />
+                    {currentTable[key].name}
+                </label>
+
             })}
-            <Link to={getGameRoute(PAGES.pa10)}
-            onClick={()=>{
-                localStorage.setItem('points', JSON.stringify(points))
-            }}
-            >Dalej</Link>
-        </div>
+        </form>
+
+
     );
 }
