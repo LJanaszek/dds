05c737028279ab30b4d81ed824d719ec1894bd6a
LJanaszek <lukasjanaszek@gmail.com>
2024-12-22 14:38:13 +0100

    dodanie grafik

--- src/components/map/pixi-app/app.ts
+++ src/components/map/pixi-app/app.ts
@@ -37,7 +37,14 @@ export default class PixiApp extends PIXI.Application {
         loadSprites([
             ['przychodnia', `${config.assetsPath}przychodnia.png`],
             ['pinezka', `${config.assetsPath}pinezka.png`],
+            //mapy
             ['klub', `${config.assetsPath}klub/klub.png`],
+            ['oboz', `${config.assetsPath}oboz/oboz_calosc.png`],
+            ['przychodnia', `${config.assetsPath}przychodnia/przych_calosc.png`],
+            ['szkola', `${config.assetsPath}szkola/szkola_calosc.png`],
+            ['szkolaOnline', `${config.assetsPath}szkola_online/online_calosc.png`],
+
+            //klub
             ['bieznia', `${config.assetsPath}klub/bieznia.png`],
             ['biezniaVisited', `${config.assetsPath}klub/bieznia_done.png`],
             ['boisko1' , `${config.assetsPath}klub/boisko1.png`],
@@ -50,6 +57,55 @@ export default class PixiApp extends PIXI.Application {
             ['szatnieVisited', `${config.assetsPath}klub/szatnie_done.png`],
             ['parking', `${config.assetsPath}klub/parking.png`],
             ['parkingVisited', `${config.assetsPath}klub/parking_done.png`],
+
+            //oboz
+            ['brama', `${config.assetsPath}oboz/brama.png`],
+            ['bramaVisited', `${config.assetsPath}oboz/brama_done.png`],
+            ['hustawka', `${config.assetsPath}oboz/hustawka.png`],
+            ['hustawkaVisited', `${config.assetsPath}oboz/hustawka_done.png`],
+            ['jezioro', `${config.assetsPath}oboz/jezioro.png`],
+            ['jezioroVisited', `${config.assetsPath}oboz/jezioro_done.png`],
+            ['pokoj', `${config.assetsPath}oboz/pokoj.png`],
+            ['pokojVisited', `${config.assetsPath}oboz/pokoj_done.png`],
+            ['toalety', `${config.assetsPath}oboz/toalety.png`],
+            ['toaletyVisited', `${config.assetsPath}oboz/toalety_done.png`],
+
+            //przychodnia
+            ['korytarz', `${config.assetsPath}przychodnia/przych_korytarz.png`],
+            ['korytarzVisited', `${config.assetsPath}przychodnia/przych_korytarz_done.png`],
+            ['lekarski', `${config.assetsPath}przychodnia/przych_lekarski.png`],
+            ['lekarskiVisited', `${config.assetsPath}przychodnia/przych_lekarski_done.png`],
+            ['pielegniarki', `${config.assetsPath}przychodnia/przych_pielegniarki.png`],
+            ['ppielegniarkiVisited', `${config.assetsPath}przychodnia/przych_pielegniarki_done.png`],
+            ['rejestracja', `${config.assetsPath}przychodnia/przych_rejestracja.png`],
+            ['rejestracjaVisited', `${config.assetsPath}przychodnia/przych_rejestracja_done.png`],
+            ['zabiegowy', `${config.assetsPath}przychodnia/przych_zabiegowy.png`],
+            ['zabiegowyVisited', `${config.assetsPath}przychodnia/przych_zabiegowy_done.png`],
+
+            //szkola
+            ['toalety', `${config.assetsPath}szkola/szkoal_toalet.png`],
+            ['toaletyVisited', `${config.assetsPath}szkola/szkoal_toalet_done.png`],
+            ['klasa', `${config.assetsPath}szkola/szkola_klasa.png`],
+            ['klasaVisited', `${config.assetsPath}szkola/szkola_klasa_done.png`],
+            ['korytarzSzkola', `${config.assetsPath}szkola/szkola_koryt.png`],
+            ['korytarzSzkolaVisited', `${config.assetsPath}szkola/szkoal_koryt_done.png`],
+            ['parkingSzkola', `${config.assetsPath}szkola/szkola_parking.png`],
+            ['parkingSzkolaVisited', `${config.assetsPath}szkola/szkola_parking_done.png`],
+            ['pedagog', `${config.assetsPath}szkola/szkola_pedagog.png`],
+            ['pedagogVisited', `${config.assetsPath}szkola/szkola_pedagog_done.png`],
+
+            //szkola online
+            ['chat', `${config.assetsPath}szkola_online/chat.png`],
+            ['chatVisited', `${config.assetsPath}szkola_online/chat_done.png`],
+            ['gklasowa', `${config.assetsPath}szkola_online/gklasowa.png`],
+            ['gklasowaVisited', `${config.assetsPath}szkola_online/gklasowa_done.png`],
+            ['samochod', `${config.assetsPath}szkola_online/samochod.png`],
+            ['samochodVisited', `${config.assetsPath}szkola_online/samochod_done.png`],
+            ['spotkanie', `${config.assetsPath}szkola_online/spotkanie.png`],
+            ['spotkanieVisited', `${config.assetsPath}szkola_online/spotkanie_done.png`],
+            ['wakacje', `${config.assetsPath}szkola_online/wakacje.png`],
+            ['wakacjeVisited', `${config.assetsPath}szkola_online/wakacje_done.png`],
+
         ]).then(() => {
             this.initApp();
         });
